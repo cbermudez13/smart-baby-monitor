4482c32a642fddb10519e6d76ef9ff54d16ce14c
cbermudez13 <cbermudezrivas@gmail.com>
2022-10-29 15:05:32 -0400

    Amplify PubSub implementation for pulling data from sensors on iot core

--- src/screens/SensorScreen/SensorScreen.js
+++ src/screens/SensorScreen/SensorScreen.js
@@ -1,82 +1,34 @@
-import Amplify, { Auth, API } from 'aws-amplify';
 import React from 'react';
-import awsconfig from './aws-exports';
-import '@aws-amplify/ui/dist/style.css';
-import { PubSub } from 'aws-amplify';
-import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+import { Amplify, PubSub, Auth } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub';
+import awsmobile from '../../aws-exports'
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsmobile);
 
 var SUB_TOPIC = "esp32/pub";
 var PUB_TOPIC = "esp32/sub";
 
 // Apply plugin with configuration
 Amplify.addPluggable(new AWSIoTProvider({
-  aws_pubsub_region: '<YOUR-IOT-REGION>',
-  aws_pubsub_endpoint: 'wss://xxxxxxxxxxxxx.iot.<YOUR-IOT-REGION>.amazonaws.com/mqtt',
+  aws_pubsub_region: 'us-east-1',
+  aws_pubsub_endpoint: 'wss://aoa59ba33x299-ats.iot.us-east-1.amazonaws.com/mqtt',
 }));
 
-async function ProcessMessage(payload) {
-  console.log('Message received', payload);
-  let topic=payload.value[Object.getOwnPropertySymbols(payload.value)[0]];
-  let time=payload.value.time;
-  let sensor_a0=payload.value.sensor_a0;
-  let scrollBox = document.getElementById('incomingMsg');
-  scrollBox.innerHTML += "<b>NEW MESSAGE: </b><br></br> Topic: " + topic + "<br></br> Time: " + time + "<br></br> Sensor_a0: " + sensor_a0 + "<br></br>";
-  scrollBox.scrollTop = scrollBox.scrollHeight;
+Auth.currentCredentials().then((info) => {
+  const cognitoIdentityId = info.identityId;
+  console.log(cognitoIdentityId);
+});
+
+
+PubSub.subscribe(SUB_TOPIC).subscribe({
+  next: data => console.log('Message received', data.value),
+  error: error => console.error(error),
+  complete: () => console.log('Done'),
+});
+
+const SensorScreen = () => {
+  
+    
 }
 
-async function SendMessage() {
-  let payload=document.getElementById('msg').value;
-  document.getElementById('msg').value='';
-  console.log(payload);
-  await PubSub.publish(PUB_TOPIC, { msg: payload });
-  document.getElementById('returnMsg').innerHTML = '<h3 style="color: green">Message sent!</h3>';
-  let sentMsgBox = document.getElementById('sentMsg');
-  sentMsgBox.innerHTML += payload + "<br></br>";
-  sentMsgBox.scrollTop = sentMsgBox.scrollHeight;
-}
-
-function App() {
-  subscribe();
-  return (
-    <div className="App">
-      <div className="mt-5 row" style={{"background-color": "black", "align-items": "center", "justify-content": "center"}}>
-        <img src={logo} style={{"height": "5vmin"}} alt="logo"></img>
-        <h1 style={{"color": "white"}}>IoT Messaging</h1>
-        <a href="https://aws.amazon.com/what-is-cloud-computing" className="pl-4"><img src="https://d0.awsstatic.com/logos/powered-by-aws-white.png" alt="Powered by AWS Cloud Computing"></img></a>
-      </div>
-      <div className="row">
-        <div id="publisher" className="col ml-5 mt-5 mb-5 mr-3" style={{"border-style": "solid", "border-width": "2px"}}>
-          <h2>Publisher</h2>
-          <p>The box below can be used to publish messages back to your devices by publishing to the topic <b>{PUB_TOPIC}</b></p>
-          <h5>Message: </h5>
-          <input type="text" className="form-control" id="msg" name="msg" placeholder="Enter Message Here"></input>
-          <br></br>
-          <button className="btn btn-primary" onClick={SendMessage}>Send Message</button>
-          <div id='returnMsg'></div>
-          <br></br>
-          <h3>Sent Messages:</h3>
-          <p>Your sent messages will appear here</p>
-          <div id='sentMsg' className="overflow-auto mb-5 border" syle={{"max-height": "220px"}}></div>
-        </div>
-        <br></br><br></br>
-        <div id="subscriber" className="col mt-5 mr-5 mb-5" style={{"border-style": "solid", "border-width": "2px"}}>
-          <h2>Subscriber</h2>
-          <p>New messages from your device(s) that publish to the topic <b>{SUB_TOPIC}</b> will appear in the box below</p>
-          <div id="incomingMsg" className="overflow-auto border" style={{"max-height": "200px"}}></div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function subscribe() {
-  PubSub.subscribe(SUB_TOPIC).subscribe({
-    next: data => ProcessMessage(data),
-    error: error => console.error(error),
-    close: () => console.log('Done'),
-  });
-}
-
-export default withAuthenticator(App, true);
+export default SensorScreen;
